9463f5037d1c3a83f85448fc6d9d4e330d78dfea
gareth <gmiller290488@gmail.com>
2017-12-07 11:24:52 +0000

    inital web app - gets json from wikipedia and displays it

---
+++ javascript.js
@@ -0,0 +1,40 @@
+
+function doSearch() {
+   var val = $("#searchbar").val();
+   console.log("hello");
+   if (val.length == 0) {
+       window.location.href = "https://en.wikipedia.org/wiki/Special:Random";
+       return;
+     }
+		         
+   $.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&search=" + val + "&origin=*&utf8",function(data){
+   var name = data[1];
+   var description = data[2];
+   var link = data[3];
+   showResults(name, description, link);
+    });
+   }
+  
+function showResults(names, desc, urls) {
+   var item = ""; 
+   for (var i = 0; i < names.length; i++) {
+       item += "<div class=\"well animated fadeIn\">";
+       item += "<strong>"+ names[i] +": </strong>";
+       item += "<p>" + desc[i] + "</p>";
+       item += "<a href=\"" + urls[i] + "\"";
+       item += "<i class=\"fa fa-external-link\" aria-hidden=\"true\"></i>";
+       item += "</a>";
+       item += "</div>";
+        }
+   $("#results").html(item);
+    };  
+  
+$(document).ready(function(){
+  $(document).keypress(function(e) {
+     if(e.which == 13) {
+       doSearch();
+          }
+      });
+  $("#searchbtn").click(doSearch);
+});
+ 
