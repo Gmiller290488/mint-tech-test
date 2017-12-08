98eddbcb28b71820f5af47339e08eb1c199b0336
gareth <gmiller290488@gmail.com>
2017-12-08 09:07:33 +0000

    autocomplete works and so do the links - need to sort formatting

--- javascript.js
+++ javascript.js
@@ -1,67 +1,49 @@
 
-function doSearch() {
-   var val = $("#searchbar").val();
-   $('.heading').addClass('hide');
-   $('.search-br').addClass('hide');
-   $('.search-area').addClass('search-area-top');
-   $('.search-area').removeClass('search-area');
-		         
-   $.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&search=" + val + "&origin=*&utf8",function(data){
-   var name = data[1];
-   var description = data[2];
-   var link = data[3];
-   showResults(name, description, link, val);
-    });
-   }
-  
-function showResults(names, desc, urls, val) {
-   var item = ""; 
-   for (var i = 0; i < names.length; i++) {
-       item += "<div id=\"result-div\" class=\"well animated fadeIn\">";
-       item += "<strong>"+ names[i] +": </strong>";
-       item += "<p>" + desc[i] + "</p>";
-       item += "<a data-target=\"#results\" href=\"" + urls[i] + "\"";
-       item += "<i class=\"fa fa-external-link\" aria-hidden=\"true\"></i>";
-       item += "</a>";
-       item += "</div>";
-        }
-   $("#results").html(item);
-      
-   $('[data-target]').click( function (e) {
-     window.scrollTo(0, 0);
-     $('#result-div').addClass('hide'); 
-     var target = $($(this).attr('data-target'));
-     target.load($(this).attr('href'));
-     e.preventDefault(); // prevent anchor from changing window.location
-   $('#results').addClass('results-container-white');
-   $('#backbtn').addClass('backbtn-show');
-   $('#backbtn').removeClass('backbtn');
-   $('#searchbar').addClass('hide');
-   $('#searchbtn').addClass('hide');
-
-
-   $('#backbtn').click( function (e) { 
-      window.scrollTo(0,0); 
-      $('#results').removeClass('results-container-white');
-      $('#result-div').removeClass('hide');    
-      $('#backbtn').removeClass('backbtn-show');
-      $('#backbtn').addClass('backbtn');
-      $('#searchbar').removeClass('hide');
-      $('#searchbtn').removeClass('hide');
-      doSearch();
-   });
-       
-       });
-       };
 
 
 
 $(document).ready(function(){
-  $(document).keypress(function(e) {
-     if(e.which == 13) {
-       doSearch();
-          }
+   var input = document.querySelector('input');
+   var teams = ['arsenal', 'chelsea', 'liverpool', 'manchester city', 'manchester united'];
+   var results;
+   var wikiurlstart = "https://en.wikipedia.org/wiki/";
+   var wikiurlend = "_fc";
+
+function autocomplete(val) {
+   var team_return = [];
+   for (i =0; i < teams.length; i++) {
+      if (val === teams[i].slice(0, val.length)) {
+         team_return.push(teams[i]);
+      }
+   }
+   return team_return;
+}
+
+input.onkeyup = function(e) {
+   input_val = this.value;
+   if (input_val.length > 0) {
+      var teams_to_show = [];
+
+      autocomplete_results = document.getElementById("autocomplete-results");
+      autocomplete_results.innerHTML = '';
+      teams_to_show = autocomplete(input_val);
+
+      for (i = 0; i < teams_to_show.length; i++) {
+	  autocomplete_results.innerHTML += '<li><a data-target=\"#results\" href=' + wikiurlstart + teams_to_show[i] + wikiurlend + '>' + teams_to_show[i] + '</a></li>';
+      }
+      autocomplete_results.style.display = 'block';
+      $('[data-target]').click( function (e) {
+          window.scrollTo(0, 0);
+	  $('#result-div').addClass('hide'); 
+	  var target = $($(this).attr('data-target'));
+	  target.load($(this).attr('href'));
+	  e.preventDefault(); // prevent anchor from changing window.location
       });
-  $("#searchbtn").click(doSearch);
-});
+   } else {
+     teams_to_show = [];
+     autocomplete_results.innerHTML = '';
+   }
+}
+
+      });
  
