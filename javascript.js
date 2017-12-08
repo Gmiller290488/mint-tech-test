8ffcd3a9cc4129a3e12432f1d4c69c9f03aa145f
gareth <gmiller290488@gmail.com>
2017-12-08 11:56:10 +0000

    added the rest of the premier league clubs and added a clause to catch bastard AFC Bournemouth

--- javascript.js
+++ javascript.js
@@ -1,10 +1,6 @@
-
-
-
-
 $(document).ready(function(){
    var input = document.querySelector('input');
-   var teams = ['arsenal', 'chelsea', 'liverpool', 'manchester city', 'manchester united'];
+   var teams = ['Arsenal', 'Tottenham Hotspur', 'Burnley', 'Watford', 'Leicester City', 'Everton', 'Southampton', 'Brighton & Hove Albion', 'Stoke', 'Bournemouth', 'Newcastle United', 'Huddersfield', 'West Bromwich Albion', 'Crystal Palace', 'West Ham United', 'Swansea', 'Chelsea', 'Liverpool', 'Manchester City', 'Manchester United'];
    var results;
    var wikiurlstart = "https://en.wikipedia.org/wiki/";
    var wikiurlend = "_fc";
@@ -12,7 +8,7 @@ $(document).ready(function(){
 function autocomplete(val) {
    var team_return = [];
    for (i =0; i < teams.length; i++) {
-      if (val === teams[i].slice(0, val.length)) {
+      if (val == teams[i].slice(0, val.length).toUpperCase() || val == teams[i].slice(0, val.length).toLowerCase()) {
          team_return.push(teams[i]);
       }
    }
@@ -23,13 +19,17 @@ input.onkeyup = function(e) {
    input_val = this.value;
    if (input_val.length > 0) {
       var teams_to_show = [];
-
+      var teams_to_show_url = [];
       autocomplete_results = document.getElementById("autocomplete-results");
       autocomplete_results.innerHTML = '';
       teams_to_show = autocomplete(input_val);
-
       for (i = 0; i < teams_to_show.length; i++) {
-	  autocomplete_results.innerHTML += '<li><a data-target=\"#results\" href=' + wikiurlstart + teams_to_show[i] + wikiurlend + '>' + teams_to_show[i] + '</a></li>';
+	      if (teams_to_show[i] === 'Bournemouth') {
+		      teams_to_show_url[i] = 'A.F.C._Bournemouth';}
+         else {
+          teams_to_show_url[i]= (teams_to_show[i].split(' ').join('_')) + '_F.C.';
+	 }
+	  autocomplete_results.innerHTML += '<li class="link"><a data-target=\"#results\" href=' + wikiurlstart + teams_to_show_url[i] + '>' + teams_to_show[i] + '</a></li>';
       }
       autocomplete_results.style.display = 'block';
       $('[data-target]').click( function (e) {
@@ -38,9 +38,15 @@ input.onkeyup = function(e) {
 	  var target = $($(this).attr('data-target'));
 	  target.load($(this).attr('href'));
 	  e.preventDefault(); // prevent anchor from changing window.location
+          $('#results').addClass('results-container-white');
+          $('.heading').addClass('hide');
+	  $('.search-area').addClass('search-area-top');
+	  $('.search-area').removeClass('search-area');
+	  $('.link').addClass('hide');
       });
    } else {
      teams_to_show = [];
+     teams_to_show_url = [];
      autocomplete_results.innerHTML = '';
    }
 }
